d7373f180621385b9be893396ac07355e2572fd5
luchy 112 <oluchichuks376@gmail.com>
2025-03-16 12:44:02 +0100

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from "./pages/App";
+
+
+
+
+
+const rootElement = document.getElementById('root')as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+        <React.StrictMode>
+                <App/>
+        </React.StrictMode>
+    );
+  
+
